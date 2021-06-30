d1cf26565978e5dbc7d1f155687bf78e30ff61dc
Uwe L. Korn <uwe.korn@quantco.com>
2021-06-30 17:49:12 +0200

    Split into subpackages

--- recipe/install_library.bat
+++ recipe/install_library.bat
@@ -8,3 +8,43 @@ echo on
 
 cmake --build . --target install
 if errorlevel 1 exit 1
+
+if [%PKG_NAME%] == [libbrotlicommon] (
+  del %LIBRARY_PREFIX%\bin\brotli.exe
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\brotlidec.lib
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\bin\brotlidec.dll
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\pkgconfig\libbrotlidec.pc
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\brotlienc.lib
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\bin\brotlienc.dll
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\pkgconfig\libbrotlienc.pc
+  if errorlevel 1 exit 1
+) else if [%PKG_NAME%] == [libbrotlienc] (
+  del %LIBRARY_PREFIX%\bin\brotli.exe
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\brotlidec.lib
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\bin\brotlidec.dll
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\pkgconfig\libbrotlidec.pc
+  if errorlevel 1 exit 1
+) else if [%PKG_NAME%] == [libbrotlidec] (
+  del %LIBRARY_PREFIX%\bin\brotli.exe
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\brotlienc.lib
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\bin\brotlienc.dll
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\lib\pkgconfig\libbrotlienc.pc
+  if errorlevel 1 exit 1
+)
+
+if NOT [%PKG_NAME%] == [brotli] (
+  del %LIBRARY_INC%\brotli
+  if errorlevel 1 exit 1
+)
