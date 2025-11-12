9aceb139522107e0fcc0df8f2846ae282193cfd9
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-11-12 12:50:11 +0100

    Add debugging output to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,5 +13,7 @@ cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
 
 ninja
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+  echo "Klaus' debug qemu ld prefix: ${QEMU_LD_PREFIX}"
+  ls ${QEMU_LD_PREFIX}
   ctest -V
 fi
