813a57b4f6c9ef46d3e7a97d4e8b5e859ffe0f65
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-11-12 18:13:15 +0100

    Sort out build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,18 +3,18 @@
 set -exuo pipefail
 
 BROTLI_CFLAGS="-O3"
+if [[ "${QEMU_LD_PREFIX:-}" != "" ]]; then
+  EXTRA_CMAKE_FLAGS=-DBROTLI_WRAPPER_LD_PREFIX="${QEMU_LD_PREFIX:-}"
+fi
 
 # Build both static and shared libraries
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_C_FLAGS=$BROTLI_CFLAGS \
       -GNinja \
-      -DCMAKE_BUILD_TYPE=Release \
+      ${EXTRA_CMAKE_FLAGS} \
       .
 
 ninja
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  if [[ "${QEMU_LD_PREFIX:-}" != "" ]]; then
-    EXTRA_CTEST_FLAGS=-DBROTLI_WRAPPER_LD_PREFIX="${QEMU_LD_PREFIX:-}"
-  fi
   ctest -V ${EXTRA_CTEST_FLAGS:-}
 fi
